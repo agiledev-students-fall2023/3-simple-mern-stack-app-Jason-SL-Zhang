04daee5f486c70ce8b6bafa011c90fb03df62d65
Jason-SL-Zhang <sz3042@nyu.edu>
2023-09-27 22:39:06 -0400

    UpdateAboutUs

--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -9,9 +9,12 @@ const AboutUs = props => {
       <h1>About Us</h1>
       <p>
         Hello, my name is Jason Zhang, and I am a senior at NYU CAS majoring in Economics and Computer Science. I was born in Wenzhou, China, 
-        and moved to Great Neck, New York, when I was ten years old. I have a strong interest in sports, particularly basketball and football 
-        because I find these sports to be a source of immense excitement and camaraderie. My favorite NBA team is the Brooklyn Nets, and my 
-        favorite NFL team is the Buffalo Bills. In addition to sports, I enjoy playing League of Legends, watching movies, and traveling.
+        and moved to Great Neck, New York, when I was ten years old. 
+      </p>
+      <p>
+        I have a strong interest in sports, particularly basketball and football because I find these sports to be a source of immense excitement 
+        and camaraderie. My favorite NBA team is the Brooklyn Nets, and my favorite NFL team is the Buffalo Bills. In addition to sports, I enjoy 
+        playing League of Legends, watching movies, and traveling.
       </p>
       <img src ={photo} alt = "Description" className = "image"/>
     </>
