8fc9591cdf547566f5195f98974e38306c552cdd
Jason-SL-Zhang <sz3042@nyu.edu>
2023-09-27 22:50:24 -0400

    UpdateAboutUs

--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -7,7 +7,6 @@ const AboutUs = props => {
   return (
     <>
       <h1>About Us</h1><br></br>
-      <p>text-align: left</p>
       <p>
         Hello, my name is Jason Zhang, and I am a senior at NYU CAS majoring in Economics and Computer Science. I was born in Wenzhou, China, 
         and moved to Great Neck, New York, when I was ten years old.
