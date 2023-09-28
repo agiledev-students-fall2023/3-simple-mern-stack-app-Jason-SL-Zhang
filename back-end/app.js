ed7679fd6e2db03914bcd01b2594f755719719af
Jason-SL-Zhang <sz3042@nyu.edu>
2023-09-28 12:50:17 -0400

    UpdateAboutUs

--- back-end/app.js
+++ back-end/app.js
@@ -40,6 +40,23 @@ app.get('/messages', async (req, res) => {
   }
 })
 
+app.get('/about-us', async (req, res) => {
+  // load all messages from database
+  try {
+    const aboutData = await AboutInfo.findOne({})
+    res.json({
+      aboutData: aboutData,
+      status: 'all good',
+    })
+  } catch (err) {
+    console.error(err)
+    res.status(400).json({
+      error: err,
+      status: 'failed to retrieve messages from the database',
+    })
+  }
+})
+
 // a route to handle fetching a single message by its id
 app.get('/messages/:messageId', async (req, res) => {
   // load all messages from database
