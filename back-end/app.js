81ebd37d2c248e2200cbcfae483aea5afe586cb0
Jason-SL-Zhang <sz3042@nyu.edu>
2023-10-02 00:46:13 -0400

    UpdateAboutUs

--- back-end/app.js
+++ back-end/app.js
@@ -83,7 +83,10 @@ app.get('/about-us', (req, res) => {
   try {
     const aboutUsData = {
       paragraphs: [
-        "Hello, my name is Jason Zhang, and I am a senior at NYU CAS majoring in Economics and Computer Science. I was born in Wenzhou, China, and moved to Great Neck, New York, when I was ten years old. I have a strong interest in sports, particularly basketball and football because I find these sports to be a source of immense excitement and camaraderie. My favorite NBA team is the Brooklyn Nets, and my favorite NFL team is the Buffalo Bills. In addition to sports, I enjoy playing League of Legends, watching movies, and traveling."
+        "Hello, my name is Jason Zhang, and I am a senior at NYU CAS majoring in Economics and Computer Science.",
+        "I was born in Wenzhou, China, and moved to Great Neck, New York, when I was ten years old. I have a strong interest in sports,",
+        "particularly basketball and football because I find these sports to be a source of immense excitement and camaraderie.",
+        "My favorite NBA team is the Brooklyn Nets, and my favorite NFL team is the Buffalo Bills. In addition to sports, I enjoy playing League of Legends, watching movies, and traveling."
       ],
       image: '/photo.jpg',
     };
