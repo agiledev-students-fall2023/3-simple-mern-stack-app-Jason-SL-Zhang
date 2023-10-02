e223312b358eb59873636004059f5067f5ef5aca
Jason-SL-Zhang <sz3042@nyu.edu>
2023-10-01 23:11:07 -0400

    UpdateAboutUs

--- back-end/app.js
+++ back-end/app.js
@@ -41,22 +41,13 @@ app.get('/messages', async (req, res) => {
   }
 })
 
-app.get('/about-us', async (req, res) => {
-  // load all messages from database
-  try {
-    const aboutData = {paragraph: ["Hello, my name is Jason Zhang, and I am a senior at NYU CAS majoring in Economics and Computer Science. I was born in Wenzhou, China, and moved to Great Neck, New York, when I was ten years old."]};
-    res.json({
-      aboutData: aboutData,
-      status: 'all good',
-    })
-  } catch (err) {
-    console.error(err)
-    res.status(400).json({
-      error: err,
-      status: 'failed to retrieve messages from the database',
-    })
-  }
-})
+app.get('/about-us', (req, res) => {
+  res.json({ 
+    name: "Jason Zhang",
+    description: "Hello, my name is Jason Zhang, and I am a senior at NYU CAS majoring in Economics and Computer Science. I was born in Wenzhou, China, and moved to Great Neck, New York, when I was ten years old. I have a strong interest in sports, particularly basketball and football because I find these sports to be a source of immense excitement and camaraderie. My favorite NBA team is the Brooklyn Nets, and my favorite NFL team is the Buffalo Bills. In addition to sports, I enjoy playing League of Legends, watching movies, and traveling.",
+    imageURL: "http://localhost:5002/static/images/photo.jpg"
+  });
+});
 
 // a route to handle fetching a single message by its id
 app.get('/messages/:messageId', async (req, res) => {
