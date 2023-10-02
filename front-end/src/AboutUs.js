5ad0bba8b67b45ce40c4a0083314732ad6880c09
Jason-SL-Zhang <sz3042@nyu.edu>
2023-10-02 01:37:29 -0400

    UpdateAboutUs

---
+++ front-end/src/AboutUs.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import './AboutUs.css'
+
+const AboutUs = props => {
+    const [paragraph, setParagraph] = useState('')
+    const [photo, setPhoto] = useState('')
+    const [error, setError] = useState('')
+
+    const fetchAboutUs = () => {
+      axios
+        .get(`${process.env.REACT_APP_SERVER_HOSTNAME}/aboutus`)
+        .then(response => {
+            console.log(response)
+            setParagraph(response.data.paragraph)
+            setPhoto(response.data.photo)
+        })
+        .catch(err => {
+            const errMsg = JSON.stringify(err, null, 2) 
+            setError(errMsg)
+        })
+    }
+
+    useEffect(() => {
+        
+        fetchAboutUs()
+    
+      }, [])
+
+    return (
+      <>
+        {error && <p className = "Messages-error">{error}</p>}
+        <text> {paragraph}
+        </text>
+        <br></br>
+        <img src = {photo} alt = "me" />
+      </>
+    )
+  }
+
+// make this component available to be imported into any other file
+export default AboutUs
